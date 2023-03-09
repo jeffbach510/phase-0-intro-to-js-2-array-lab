d0f69044926ec46fc0f05e46441fbad026051d1f
Jeffrey Bachman <jeffbach510@sbcglobal.net>
2023-03-08 18:12:31 -0800

    changes to index.js

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+};
+
+destructivelyAppendCat("Ralph");
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+};
+
+destructivelyPrependCat("Bob");
+
+function destructivelyRemoveLastCat()  {
+    cats.pop();
+};
+
+destructivelyRemoveLastCat();
+
+function destructivelyRemoveFirstCat()  {
+    cats.shift();
+};
+
+destructivelyRemoveFirstCat();
+
+function appendCat() {
+    return[...cats, "Broom"];
+};
+
+appendCat();
+
+function prependCat(name) {
+    return[name, ...cats];
+};
+
+prependCat("Broom");
+
+function removeLastCat()  {
+    return cats.slice(0,cats.length-1);
+};
+removeLastCat();
+
+function removeFirstCat()  {
+    return cats.slice(1)
+};
+removeFirstCat();
